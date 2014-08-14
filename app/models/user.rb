7cb5bfcbe9ef0a76fa8178d0c4d460eb3714117e
Charle Walker <charliehwalker@gmail.com>
2014-08-13 21:23:45 -0400

    Updating user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,46 +1,9 @@
-class ActiveRecord::Base
-  has_attached_file :avatar
-  # Validate content type
-  validates_attachment_content_type :avatar, :content_type => /\Aimage/
-  # Validate filename
-  validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
-  # Explicitly do not validate
-  do_not_validate_attachment_file_type :avatar
-end
-
 class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-<<<<<<< HEAD
   has_attached_file :avatar, :styles => { :medium => "300x300", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
-=======
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :download,
-                    :storage => :s3,
-                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
-
-  def s3_credentials
-    {:bucket => "xxx", :access_key_id => "xxx", :secret_access_key => "xxx"}
-  end
-end
-
-class Avatar < ActiveRecord::Base
-  has_attached_file :document, styles: {thumbnail: "60x60#"}
-  validates_attachment :document, content_type: { content_type: "application/pdf" }
-  validates_something_else # Other validations that conflict with Paperclip's
->>>>>>> faf38263ffdbbc1f02373e0167e01390eca1e664
-end
-class Message < ActiveRecord::Base
-  has_attached_file :asset, styles: {thumb: "100x100#"}
-
-  before_post_process :skip_for_audio
-
-  def skip_for_audio
-    ! %w(audio/ogg application/ogg).include?(asset_content_type)
-  end
 end
