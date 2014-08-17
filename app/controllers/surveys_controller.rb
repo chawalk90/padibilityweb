f994cf8817fcc68a33429bf2a1bc88ba9390e4bf
Charle Walker <charliehwalker@gmail.com>
2014-08-17 13:14:38 -0400

    another survey action

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -25,4 +25,9 @@ private
     	params.require(:survey).permit(:first_name, :last_name, :email, :looking_for, :moving_to, :gender, :coed, :age, :roommate_type, :housing_type, :roommates_estimate, :roommates_amount, :roommates_group, :roommates_names, :max_rent, :move_in, :move_out, :bedrooms, :amenities, :apartment_pet, :roommate_pet, :hometown, :school, :company, :terms, :avatar, :wake_up, :bedtime, :smoke, :smokeoften, :smokesocially, :smokequit, :drink, :drinkoften, :drinksocially, :drinkquit, :drugs, :drugsoften, :drugssocially, :drugsquit, :interest, :sexualactivity, :sexprivacy, :roommatesexprivacy, :overnight, :overnightoften, :roommateovernight, :realty, :availability, :rentprice, :address, :otherroom, :age_min, :age_max, :age_mode, :pad_photo, :user_status, :sociability, :tidiness, :question, :noise, :political, :religion)
     end
 
+    def idcheck
+    @survey.user_id=current_user.id
+    @survey.save
+  end
+
 end
