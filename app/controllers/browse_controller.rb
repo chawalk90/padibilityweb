a6fb4341bd76893fe387cc5142098d276e9ce67c
Charle Walker <charliehwalker@gmail.com>
2014-08-20 17:42:14 -0400

    change seekers and owners

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -3,8 +3,8 @@ def browse_params
       params.require(:browse).permit(:name)
     end
 	def seekers
-		@browse = Browse.new(browse_params)
-    	@browse.save
+		@browse = Browse.new
+    	# @browse.save
 	end
 	def owners
 		@browse = Browse.new(browse_params)
