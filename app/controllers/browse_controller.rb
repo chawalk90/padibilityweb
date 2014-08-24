bc2252b0e6fb1f30f89229b20a3eb94782864562
Charle Walker <charliehwalker@gmail.com>
2014-08-24 12:09:08 -0400

    add new method browse controller

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,13 +1,20 @@
 class BrowseController < ApplicationController
 def browse_params
       params.require(:browse).permit(:name)
-    end
-	def seekers
+end
+def new
+	@browse = Browse.new
+end
+    def owners
+		@browse = Browse.new(browse_params)
+		@survey.user_id=current_user.id
+    	@browse.save
+	end
+	def seeker
 		@browse = Browse.new(browse_params)
     	@browse.save
 	end
-	def owner
-		@browse = Browse.new(browse_params)
-    	@browse.save
+
+	def idcheck
 	end
 end
