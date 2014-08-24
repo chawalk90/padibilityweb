536bc8843d94ee9ba5fdccb0098642db26a83f2f
Charle Walker <charliehwalker@gmail.com>
2014-08-24 13:18:33 -0400

    edit browse params method browse controller

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,10 +1,15 @@
 class BrowseController < ApplicationController
-def browse_params
-      params.require(:browse).permit(:name)
-end
-def new
-	@browse = Browse.new
-end
+	#before_filter :new, :create, :survey_params, :owners, :seeker, :survey_params, :idcheck
+	def browse_params
+      browse_params.require(:browse).permit(:name)
+	end
+	def new
+		@browse = Browse.new
+	end
+	def create
+		@browse = Browse.new(browse_params)
+	end
+	private
     def owners
 		@browse = Browse.new(browse_params)
 		@survey.user_id=current_user.id
