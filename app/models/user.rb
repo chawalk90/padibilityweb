6f127a98bd2a36c1c0e7ed7876c786c39adc2bb4
Charle Walker <charliehwalker@gmail.com>
2014-08-28 13:29:47 -0400

    revise user rb attr

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,5 @@
 class User < ActiveRecord::Base
-	devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_attached_file :avatar, :styles => { :medium => "300x300", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, 
-  :content_type => {:content_type => ["image/jpeg", "image/gif", "image/png"]}
-  	has_one :survey
+  attr_accessible :avatar
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
