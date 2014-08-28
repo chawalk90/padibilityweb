3951cbb6ee42abed3de71251d9416a4f7f5a3403
Charle Walker <charliehwalker@gmail.com>
2014-08-28 11:09:24 -0400

    survey where instance browse controller

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,9 +1,9 @@
 	class BrowseController < ApplicationController
 		#before_filter :new, :create, :survey_params, :owners, :seeker, :survey_params, :idcheck
 	    def owners
-			@surveys = Survey.where([:user_status] = "owner", 1)
+			@surveys = Survey.where(@user_status = "owner", 1)
 		end
 		def seekers
-			@surveys = Survey.where([:user_status] = "seeker", 0)
+			@surveys = Survey.where(@user_status = "seeker", 0)
 		end
 	end
