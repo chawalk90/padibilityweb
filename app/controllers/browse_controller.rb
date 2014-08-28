2a52a65f1f82c1ba831e32af3811144f89d17f5e
Charle Walker <charliehwalker@gmail.com>
2014-08-28 11:21:40 -0400

    params user status browse controlller

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,9 +1,9 @@
 	class BrowseController < ApplicationController
 		#before_filter :new, :create, :survey_params, :owners, :seeker, :survey_params, :idcheck
 	    def owners
-			@surveys = Survey.where(@user_status = "owner", 1)
+			@surveys = Survey.where("user_status = ?", params[:user_status], true)
 		end
 		def seekers
-			@surveys = Survey.where(@user_status = "seeker", 0)
+			@surveys = Survey.where("user_status = ?",  params[:user_status], false)
 		end
 	end
