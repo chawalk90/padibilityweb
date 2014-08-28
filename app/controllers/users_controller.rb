2c83323394a0d393196dcb4c602e1022a82ef27f
Charle Walker <charliehwalker@gmail.com>
2014-08-28 13:53:40 -0400

    reorganize method user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,19 @@
 class UsersController < ApplicationController
+	def create
+		@user = User.create(user_params)
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:avatar. :first_name, :last_name, :email, :password, :password_confirmation)
+	end
 	def index
 	end
 	def show
 		@user=User.find(params[:id])
 	end
-	def create
-		@user = User.create( user_params )
-	end
-
-	private
 
 	# Use strong_parameters for attribute whitelisting
 	# Be sure to update your create() and update() controller methods.
-
-	def user_params
-		params.require(:user).permit(:avatar. :first_name, :last_name, :email, :password, :password_confirmation)
-	end
 end
