2f8e41dd73f035bd1cc710613ce11f4cd6c7f1f0
Charle Walker <charliehwalker@gmail.com>
2014-08-28 11:32:37 -0400

    switch owner seeker methods controller

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,9 +1,9 @@
 	class BrowseController < ApplicationController
 		#before_filter :new, :create, :survey_params, :owners, :seeker, :survey_params, :idcheck
 	    def owners
-			@surveys = Survey.where(user_status: ["seeker"])
+			@surveys = Survey.where(user_status: ["owner"])
 		end
 		def seekers
-			@surveys = Survey.where(user_status: ["owner"])
+			@surveys = Survey.where(user_status: ["seeker"])
 		end
 	end
