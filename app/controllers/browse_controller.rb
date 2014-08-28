4576ab286a530205a2b4d1954a9eb764b40dc524
Charle Walker <charliehwalker@gmail.com>
2014-08-28 11:30:36 -0400

    change where user again

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,9 +1,9 @@
 	class BrowseController < ApplicationController
 		#before_filter :new, :create, :survey_params, :owners, :seeker, :survey_params, :idcheck
 	    def owners
-			@surveys = Survey.where("user_status = #{params[:user_status]}", true)
+			@surveys = Survey.where(user_status: ["seeker"])
 		end
 		def seekers
-			@surveys = Survey.where("user_status = #{params[:user_status]}", false)
+			@surveys = Survey.where(user_status: ["owner"])
 		end
 	end
