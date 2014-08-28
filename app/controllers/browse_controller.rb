dba78b5d4b20031853488e292e08433aa72810be
Charle Walker <charliehwalker@gmail.com>
2014-08-28 11:25:20 -0400

    change user status to hash browse

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,9 +1,9 @@
 	class BrowseController < ApplicationController
 		#before_filter :new, :create, :survey_params, :owners, :seeker, :survey_params, :idcheck
 	    def owners
-			@surveys = Survey.where("user_status = seeker", params[:user_status], true)
+			@surveys = Survey.where("user_status = #{params[:user_status]}", true)
 		end
 		def seekers
-			@surveys = Survey.where("user_status = owner",  params[:user_status], false)
+			@surveys = Survey.where("user_status = #{params[:user_status]}", false)
 		end
 	end
