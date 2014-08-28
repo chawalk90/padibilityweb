cff2cb82ee9fd2bec9341f31bbafc0ef7c3bbc30
Charle Walker <charliehwalker@gmail.com>
2014-08-28 13:47:08 -0400

    revise user params user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,15 @@ class UsersController < ApplicationController
 		@user=User.find(params[:id])
 	end
 	def create
-  @user = User.create( user_params )
-end
+		@user = User.create( user_params )
+	end
 
-private
+	private
 
-# Use strong_parameters for attribute whitelisting
-# Be sure to update your create() and update() controller methods.
+	# Use strong_parameters for attribute whitelisting
+	# Be sure to update your create() and update() controller methods.
 
-def user_params
-  params.require(:user).permit(:avatar. :first_name, :last_name, :email)
-end
+	def user_params
+		params.require(:user).permit(:avatar. :first_name, :last_name, :email, :password, :password_confirmation)
+	end
 end
