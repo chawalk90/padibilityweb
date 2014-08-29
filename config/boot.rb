7677e48358ca11b3ea8ef6eb1991fd98638d8b0e
Charle Walker <charliehwalker@gmail.com>
2014-08-28 23:13:05 -0400

    boot bundle rmagick

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+ENV['BUNDLE_RMAGICK'] ||= File.expand_path('../../rmagick', __FILE__)
+
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_RMAGICK'])
