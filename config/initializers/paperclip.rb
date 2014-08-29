0c8b06e620bb5e8431ed1c775850cbae60e3e007
Charle Walker <charliehwalker@gmail.com>
2014-08-28 22:50:08 -0400

    add avatar list item nav bar

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,5 @@
 # config/initializers/paperclip.rb
 Paperclip::Attachment.default_options['uploader.s3.amazonaws.com'] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+Paperclip::Attachment.default_options['herokuapp'] = 's3-us-west-2.amazonaws.com'
+user.avatar.url #=> http://herokuapp.s3.amazonaws.com/...
