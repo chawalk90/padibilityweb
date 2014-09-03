ef1247b85b54b55cf6c6db9a7578412ae6602bdb
Charle Walker <charliehwalker@gmail.com>
2014-09-03 17:52:13 -0400

    change version schema rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828004106) do
+ActiveRecord::Schema.define(version: 20140827230909) do
 
   create_table "surveys", force: true do |t|
     t.string   "first_name"
