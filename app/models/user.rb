45a8dbb9377a4b69ac102af348ce1af3d7ba08ff
Charle Walker <charliehwalker@gmail.com>
2014-09-03 12:05:26 -0400

    edit user rb attr file stack overflow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ActiveRecord::Base
-	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-
-	has_attached_file :avatar, styles: { 
-		thumb: '100x100>',
-    	square: '200x200#',
-    	medium: '300x300>'
-		}
-		# , :default_url => "/images/:style/missing.png"
-	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+	attr_accessible :name, :email, :password, :password_confirmation,
+                  :avatar,
+                  :avatar_file_name,
+                  :avatar_content_type,
+                  :avatar_file_size,
+                  :avatar_updated_at
+  	has_secure_password
+  	has_attached_file :avatar, :styles => { :large => "120x120>", :medium => "48x48>", :thumb => "26x26>" }
+  	before_save { |user| user.email = email.downcase }
+ 	validates :name,  presence: true, length: { maximum: 50 }
+  	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
+  	validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  	validates :password, presence: true, length: { minimum: 6 }
+  	validates :password_confirmation, presence: true
 end
