f3da8e919ca8f201f2a503954e08ff2f3f3ce57b
Charle Walker <charliehwalker@gmail.com>
2014-09-03 21:52:24 -0400

    password migration complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828004106) do
+ActiveRecord::Schema.define(version: 20140904014157) do
 
   create_table "surveys", force: true do |t|
     t.string   "first_name"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20140828004106) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
