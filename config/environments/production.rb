895ae710e4989e7850c91831888c4fecea75f164
Charle Walker <charliehwalker@gmail.com>
2014-09-04 14:57:47 -0400

    update paperclip production rb key values

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,9 +7,9 @@ Rails.application.configure do
 config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :bucket => ENV['padibility'],
+    :access_key_id => ENV['AKIAJR566VGPOWTPZLTQ'],
+    :secret_access_key => ENV['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']
   }
 }
 
