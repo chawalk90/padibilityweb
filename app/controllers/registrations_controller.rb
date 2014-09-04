4e5efe0254c84b3434dafd9e561498f92f30eba8
Charle Walker <charliehwalker@gmail.com>
2014-09-03 21:59:37 -0400

    add registrations controller with new code

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,17 @@
+  class RegistrationsController < Devise::RegistrationsController
+
+    private
+
+    def sign_up_params
+      user_params
+    end
+
+    def account_update_params
+      user_params
+    end
+
+    def user_params
+      params.require(:user).permit(arams.require(:user).permit(:avatar, :first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+  end
