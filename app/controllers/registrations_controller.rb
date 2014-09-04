2378ae26b15656c47c1355137076976c91fcc516
Charle Walker <charliehwalker@gmail.com>
2014-09-03 22:02:28 -0400

    edit create registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,13 @@
     private
 
     def create
+      @user = User.new(user_params)
+
+      if @user.save
+          redirect_to @user, notice: 'User was successfully created.'
+      else
+          render action: 'new'
+      end
     end
 
     def sign_up_params
