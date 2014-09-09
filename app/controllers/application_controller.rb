5a2ea0c2d72403525fcb033952f5ccc635f52219
Charle Walker <charliehwalker@gmail.com>
2014-09-09 09:51:19 -0400

    changes after google hangout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :avatar) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :avatar, :current_password) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :avatar, :first_name, :last_name) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :avatar, :current_password, :first_name, :last_name) }
   end
 
   protect_from_forgery with: :exception
