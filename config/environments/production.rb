d58d0efff8a592a29d4c4308ff17ae94a8a87ae0
Charle Walker <charliehwalker@gmail.com>
2014-09-09 15:13:53 -0400

    fix paperclip s3 credentials values

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,9 +7,9 @@ Rails.application.configure do
 config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['padibility'],
-    :access_key_id => ENV['AKIAJR566VGPOWTPZLTQ'],
-    :secret_access_key => ENV['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']
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 
