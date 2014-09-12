e06ada076a949c2a26430491cfc6edda7922ab92
Charle Walker <charliehwalker@gmail.com>
2014-09-12 12:30:13 -0400

    re-insert paper clip defaults env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,13 +5,13 @@ Rails.application.configure do
   config.cache_classes = true
   # config/environments/production.rb
 config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['S3_BUCKET'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
